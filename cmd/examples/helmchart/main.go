c5133ef509b318a3fd47eaafc8426b0e7ce0d844
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-02-25 13:21:45 -0500

    update helmchart example to make it configurable

--- cmd/examples/helmchart/main.go
+++ cmd/examples/helmchart/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 
 	"github.com/davidmdm/halloumi/pkg/helm"
+	"golang.org/x/term"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -20,12 +22,20 @@ func main() {
 var pg []byte
 
 func run() error {
+	var values map[string]any
+
+	if !term.IsTerminal(int(os.Stdin.Fd())) {
+		if err := yaml.NewDecoder(os.Stdin).Decode(&values); err != nil {
+			return fmt.Errorf("failed to decode stdin: %w", err)
+		}
+	}
+
 	chart, err := helm.LoadChartFromZippedArchive(pg)
 	if err != nil {
 		return fmt.Errorf("failed to load chart: %w", err)
 	}
 
-	resources, err := chart.Render(os.Args[0], "default", nil)
+	resources, err := chart.Render(os.Args[0], "default", values)
 	if err != nil {
 		return fmt.Errorf("failed to render chart resources: %w", err)
 	}
