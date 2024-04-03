8d3dbb144650d8a68910fded2033b21b5b868302
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-04-03 02:07:26 -0400

    yoke: support gzip wasms

--- pkg/yoke/wasm.go
+++ pkg/yoke/wasm.go
@@ -1,6 +1,7 @@
 package yoke
 
 import (
+	"compress/gzip"
 	"context"
 	"errors"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"slices"
 
 	"github.com/davidmdm/x/xerr"
@@ -16,7 +18,7 @@ import (
 func LoadWasm(ctx context.Context, path string) (wasm []byte, err error) {
 	uri, _ := url.Parse(path)
 	if uri.Scheme == "" {
-		return os.ReadFile(path)
+		return loadFile(path)
 	}
 
 	if !slices.Contains([]string{"http", "https"}, uri.Scheme) {
@@ -40,5 +42,42 @@ func LoadWasm(ctx context.Context, path string) (wasm []byte, err error) {
 		return nil, fmt.Errorf("unexpected statuscode fetching %s: %w", uri.String(), err)
 	}
 
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		return io.ReadAll(gzipReader(resp.Body))
+	}
+
 	return io.ReadAll(resp.Body)
+}
+
+func loadFile(path string) (result []byte, err error) {
+	if filepath.Ext(path) != ".gz" {
+		return os.ReadFile(path)
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err = xerr.MultiErrFrom("", err, file.Close())
+	}()
+
+	return io.ReadAll(gzipReader(file))
+}
+
+func gzipReader(r io.Reader) io.Reader {
+	pr, pw := io.Pipe()
+	go func() {
+		gr, err := gzip.NewReader(r)
+		if err != nil {
+			pw.CloseWithError(err)
+			return
+		}
+		if _, err := io.Copy(pw, gr); err != nil {
+			pw.CloseWithError(err)
+		}
+		pw.CloseWithError(gr.Close())
+	}()
+
+	return pr
 }
