579ca0c0452e9346f4dfd40899dd8ca4ed727916
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-04-09 21:06:20 -0400

    add yokecd releaser workflow

---
+++ cmd/internal/releaser/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/davidmdm/x/xerr"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"golang.org/x/mod/semver"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	dry := flag.Bool("dry", false, "dry-run")
+
+	var commands []string
+	flag.Func("cmd", "commands to buid as wasm and release", func(value string) error {
+		commands = append(commands, strings.Split(value, ",")...)
+		return nil
+	})
+
+	flag.Parse()
+
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		return fmt.Errorf("failed to open git repo: %w", err)
+	}
+
+	iter, err := repo.Tags()
+	if err != nil {
+		return fmt.Errorf("failed to read tags: %w", err)
+	}
+
+	versions := map[string]string{}
+
+	iter.ForEach(func(r *plumbing.Reference) error {
+		fmt.Println("detected tag:", r.Name().String())
+		release, version := path.Split(r.Name()[len("refs/tags/"):].String())
+		if !semver.IsValid(version) {
+			return nil
+		}
+		release = path.Clean(release)
+		if semver.Compare(version, versions[release]) > 0 {
+			versions[release] = version
+		}
+		return nil
+	})
+
+	releaser := Releaser{
+		Versions: versions,
+		Repo:     repo,
+		DryRun:   *dry,
+	}
+
+	var errs []error
+	for _, cmd := range commands {
+		if err := releaser.handlePath(cmd); err != nil {
+			errs = append(errs, fmt.Errorf("failed to process %s: %w", cmd, err))
+		}
+	}
+
+	return xerr.MultiErrOrderedFrom("failed to release", errs...)
+}
+
+type Releaser struct {
+	Versions map[string]string
+	Repo     *git.Repository
+	DryRun   bool
+}
+
+func (releaser Releaser) handlePath(name string) error {
+	version := releaser.Versions[name]
+
+	if version != "" {
+		tag := path.Join(name, version)
+
+		hash, err := releaser.Repo.ResolveRevision(plumbing.Revision(plumbing.NewTagReferenceName(tag)))
+		if err != nil {
+			return fmt.Errorf("failed to resolve: %s: %w", tag, err)
+		}
+
+		commit, err := releaser.Repo.CommitObject(*hash)
+		if err != nil {
+			return fmt.Errorf("failed to get commit for tag %s: %w", tag, err)
+		}
+
+		h, err := releaser.Repo.Head()
+		if err != nil {
+			return fmt.Errorf("failed to resolve head: %w", err)
+		}
+
+		head, err := releaser.Repo.CommitObject(h.Hash())
+		if err != nil {
+			return fmt.Errorf("failed to get head commit: %w", err)
+		}
+
+		headTree, err := head.Tree()
+		if err != nil {
+			return fmt.Errorf("failed to get tree for head commit: %w", err)
+		}
+
+		commitTree, err := commit.Tree()
+		if err != nil {
+			return fmt.Errorf("failed to get tree for tag commit: %w", err)
+		}
+
+		changes, err := headTree.Diff(commitTree)
+		if err != nil {
+			return fmt.Errorf("failed to diff trees: %w", err)
+		}
+
+		var changed bool
+		for _, change := range changes {
+			var (
+				from = change.From.Name
+				to   = change.To.Name
+			)
+			if strings.HasPrefix(from, "cmd/"+name+"/") || strings.HasPrefix(to, "cmd/"+name+"/") {
+				fmt.Printf("detected change from %s to %s\n", from, to)
+				changed = true
+				break
+			}
+		}
+
+		if !changed {
+			return nil
+		}
+	} else {
+		fmt.Println("No version found for", name)
+	}
+
+	outputPath, err := build(filepath.Join("cmd", name))
+	if err != nil {
+		return fmt.Errorf("failed to build wasm: %w", err)
+	}
+
+	tag := fmt.Sprintf("%s/%s", name, "v0.0.0-"+time.Now().Format("20060201150405"))
+
+	if releaser.DryRun {
+		fmt.Println("dry-run: create realease", tag)
+		return nil
+	}
+	if err := release(tag, outputPath); err != nil {
+		return fmt.Errorf("failed to release: %w", err)
+	}
+
+	return nil
+}
+
+func build(path string) (string, error) {
+	_, name := filepath.Split(path)
+
+	out := name + ".wasm"
+
+	cmd := exec.Command("go", "build", "-o", out, "./"+path)
+	cmd.Env = append(os.Environ(), "GOOS=wasip1", "GOARCH=wasm")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("%w: %s", err, output)
+	}
+	return out, nil
+}
+
+func release(tag, path string) error {
+	out, err := exec.Command("gh", "release", "create", tag, path).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%w: %s", err, out)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -7,11 +7,13 @@ require (
 	github.com/davidmdm/conf v0.0.7
 	github.com/davidmdm/x/xcontext v0.0.2
 	github.com/davidmdm/x/xerr v0.0.3
+	github.com/go-git/go-git/v5 v5.12.0
 	github.com/jedib0t/go-pretty/v6 v6.5.5
 	github.com/pmezard/go-difflib v1.0.0
 	github.com/stretchr/testify v1.9.0
 	github.com/tetratelabs/wazero v1.7.0
-	golang.org/x/term v0.18.0
+	golang.org/x/mod v0.17.0
+	golang.org/x/term v0.19.0
 	gopkg.in/yaml.v3 v3.0.1
 	helm.sh/helm/v3 v3.14.3
 	k8s.io/api v0.29.3
@@ -20,27 +22,37 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/ProtonMail/go-crypto v1.0.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.12.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
@@ -48,23 +60,29 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/skeema/knownhosts v1.2.2 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/oauth2 v0.18.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.18.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/apiextensions-apiserver v0.29.3 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
